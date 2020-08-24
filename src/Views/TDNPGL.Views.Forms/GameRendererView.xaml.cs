b2bd6d6829e7ac463a0024b2b8154582be655915
unknown <zatrit@gmail.com>
2020-08-24 15:21:30 +0300

    Fixed references

--- src/Views/TDNPGL.Views.Forms/GameRendererView.xaml.cs
+++ src/Views/TDNPGL.Views.Forms/GameRendererView.xaml.cs
@@ -1,9 +1,12 @@
 ﻿using SkiaSharp;
 using SkiaSharp.Views.Forms;
 using System;
+using System.Collections.Generic;
 using System.Reflection;
+using System.Runtime.CompilerServices;
 using TDNPGL.Core.Gameplay.Assets;
 using TDNPGL.Core.Graphics.Renderers;
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace TDNPGL.Views.Forms
@@ -15,20 +18,18 @@ namespace TDNPGL.Views.Forms
         public double height => CanvasSize.Height;
 
         public double PixelSize => ((width + height) / 2) / (Math.PI * 100);
-
         public GameRendererView()
         {
             InitializeComponent();
 
         }
-
-        public void InitGame(Assembly assembly, string GameName)
-        {
+        public void InitGame(Assembly assembly, string GameName)=>
             TDNPGL.Core.Game.Init(this, assembly, GameName, true);
-        }
         public void InitGame<EntryType>(string GameName) where EntryType : EntryPoint => InitGame(Assembly.GetAssembly(typeof(EntryType)), GameName);
 
         public SKBitmap CurrentGameBitmap { get; set; }
+
+        public override IDispatcher Dispatcher => base.Dispatcher;
 
         public void Dispose()
         {
@@ -49,6 +50,121 @@ namespace TDNPGL.Views.Forms
             }
             catch { 
             }
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+        protected override void OnPropertyChanging([CallerMemberName] string propertyName = null)
+        {
+            base.OnPropertyChanging(propertyName);
+        }
+
+        protected override void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            base.OnPropertyChanged(propertyName);
+        }
+
+        protected override void OnParentSet()
+        {
+            base.OnParentSet();
+        }
+
+        protected override void OnRequestedThemeChanged(OSAppTheme newValue)
+        {
+            base.OnRequestedThemeChanged(newValue);
+        }
+
+        protected override void OnTabIndexPropertyChanged(int oldValue, int newValue)
+        {
+            base.OnTabIndexPropertyChanged(oldValue, newValue);
+        }
+
+        protected override int TabIndexDefaultValueCreator()
+        {
+            return base.TabIndexDefaultValueCreator();
+        }
+
+        protected override void OnTabStopPropertyChanged(bool oldValue, bool newValue)
+        {
+            base.OnTabStopPropertyChanged(oldValue, newValue);
+        }
+
+        protected override bool TabStopDefaultValueCreator()
+        {
+            return base.TabStopDefaultValueCreator();
+        }
+
+        public override SizeRequest GetSizeRequest(double widthConstraint, double heightConstraint)
+        {
+            return base.GetSizeRequest(widthConstraint, heightConstraint);
+        }
+
+        protected override void InvalidateMeasure()
+        {
+            base.InvalidateMeasure();
+        }
+
+        protected override void OnChildAdded(Element child)
+        {
+            base.OnChildAdded(child);
+        }
+
+        protected override void OnChildRemoved(Element child)
+        {
+            base.OnChildRemoved(child);
+        }
+
+        protected override void OnSizeAllocated(double width, double height)
+        {
+            base.OnSizeAllocated(width, height);
+        }
+
+        protected override SizeRequest OnSizeRequest(double widthConstraint, double heightConstraint)
+        {
+            return base.OnSizeRequest(widthConstraint, heightConstraint);
+        }
+
+        protected override void ChangeVisualState()
+        {
+            base.ChangeVisualState();
+        }
+
+        public override IList<GestureElement> GetChildElements(Point point)
+        {
+            return base.GetChildElements(point);
+        }
+
+        protected override void OnBindingContextChanged()
+        {
+            base.OnBindingContextChanged();
+        }
+
+        protected override void OnPaintSurface(SKPaintGLSurfaceEventArgs e)
+        {
+            base.OnPaintSurface(e);
+        }
+
+        protected override void OnTouch(SKTouchEventArgs e)
+        {
+            base.OnTouch(e);
+        }
+
+        protected override SizeRequest OnMeasure(double widthConstraint, double heightConstraint)
+        {
+            return base.OnMeasure(widthConstraint, heightConstraint);
         }
     }
 }
