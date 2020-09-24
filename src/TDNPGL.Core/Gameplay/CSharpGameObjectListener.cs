5ec376e5b84fc4e62e71b99d7c30b84eadc7c1c6
zatrit <52380960+zatrit@users.noreply.github.com>
2020-09-24 19:01:56 +0300

    Added sound support TDNPGL.Views.Forms

---
+++ src/TDNPGL.Core/Gameplay/CSharpGameObjectListener.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Specialized;
+using TDNPGL.Core.Gameplay.Interfaces;
+
+namespace TDNPGL.Core.Gameplay
+{
+    public abstract class CSharpGameObjectListener : GameObjectListener
+    {
+        public CSharpGameObjectListener(GameObject @object)
+        {
+            Parent = @object;
+        }
+
+        public override void OnCreate() { }
+        public override void OnFirstTick() { }
+        public override void OnTick(){ }
+        public override void OnMouseReleased(SkiaSharp.SKPoint point){ }
+        public override void OnMouseReleasedOver(SkiaSharp.SKPoint point){ }
+        public override void OnCollideWith(GameObject collide) { }
+    }
+}
---
+++ src/TDNPGL.Core/Gameplay/Interfaces/IUpdateable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using TDNPGL.Core.Gameplay.LowLevel;
+
+namespace TDNPGL.Core.Gameplay.Interfaces
+{
+    public interface IUpdateable : IParentable
+    {
+        void OnTick();
+        void OnCreate();
+        void OnFirstTick();
+        void OnMouseReleased(SkiaSharp.SKPoint point);
+        void OnMouseReleasedOver(SkiaSharp.SKPoint point);
+    }
+}
