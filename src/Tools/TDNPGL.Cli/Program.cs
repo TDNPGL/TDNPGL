701f42647a69d6fdbace2b8526e614d0cff3d591
zatrit <52380960+zatrit@users.noreply.github.com>
2020-12-15 09:16:03 +0300

    Moved TDNPGL.Cli

---
+++ src/Tools/TDNPGL.Cli/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TDNPGL.Cli
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            new CLI().RunWithArgs(args);
+            if(NativeMethods.IsOwnConsole())
+            {
+                Console.Write("Press any key to exit...");
+                Console.ReadKey();
+            }
+        }
+    }
+}
