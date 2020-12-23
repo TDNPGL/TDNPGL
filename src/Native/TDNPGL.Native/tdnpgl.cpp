fb1775211bc5866bf784916a93199bf769327a4d
zatrit <zatrit@gmail.com>
2020-12-23 15:40:34 +0300

    Updated TDNPGL.Native

--- src/Native/TDNPGL.Native/tdnpgl.cpp
+++ src/Native/TDNPGL.Native/tdnpgl.cpp
@@ -1,12 +1,16 @@
 ﻿#if defined(_MSC_VER) || defined(_WIN32) || defined(WIN32)
 //  Microsoft 
-#include <game_window.h>
 #define EXPORT extern "C" __declspec(dllexport)
 #define PLATFORM "Windows"
 #elif defined(__GNUC__) || defined(__unix__ )
 //  GCC
+#include <game_window.h>
 #define EXPORT extern "C"
 #define PLATFORM "Unix"
+EXPORT int NewNativeWindow() {
+	KeyAction action = KeyAction::PRESS;
+	return (int)action;
+}
 #else
 //  do nothing and hope for the best?
 #define EXPORT
@@ -15,7 +19,10 @@
 #endif
 
 EXPORT int AABB_IsPointOver(float x, float y, float minx, float miny, float maxx, float maxy) {
-	int result = (x > minx) && (x < maxx) && (y > miny) && (y < maxy);
+	int result = (x > minx) 
+		&& (x < maxx) 
+		&& (y > miny) 
+		&& (y < maxy);
 	return result;
 }
 
