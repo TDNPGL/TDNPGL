95941c1407d18c5126c135a50060997dc52e35e1
zatrit <zatrit@gmail.com>
2020-12-23 15:42:48 +0300

    Added classes

---
+++ src/TDNPGL.Core/Graphics/RenderCallback.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TDNPGL.Core.Graphics.Renderers;
+
+namespace TDNPGL.Core.Graphics
+{
+    public delegate void RenderCallback(IGameRenderer renderer, Game game);
+}
---
+++ src/Tools/TDNPGL.Cli/Command.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Linq;
+
+namespace TDNPGL.Cli
+{
+    public class Command
+    {
+        [JsonProperty("description")]
+        public string Description;
+        [JsonProperty("name")]
+        public string Name;
+        [JsonProperty("aliases")]
+        public string[] Aliases;
+        public void PrintHelp()
+        {
+            ConsoleMethods.WriteWithColor(null, "tdnpgl " + Name, ConsoleColor.Yellow);
+            Console.WriteLine(" - \n" + Description);
+            Console.Write("Aliases: ");
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Aliases.ToList().ForEach((string str) => 
+            Console.WriteLine(str + 
+            (Aliases.Last()==str ? "" : "; ")
+            ));
+            Console.ResetColor();
+        }
+        public Command(string Description, string Name, params string[] Aliases)
+        {
+            this.Description = Description;
+            this.Name = Name;
+            this.Aliases = Aliases;
+        }
+    }
+}
