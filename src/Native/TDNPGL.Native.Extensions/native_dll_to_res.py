85ae7ad6ebabc72f29989cb02c4ff52210fd26c5
zatrit <zatrit@gmail.com>
2021-01-15 06:29:13 +0300

    Added new script

--- src/Native/TDNPGL.Native.Extensions/native_dll_to_res.py
+++ src/Native/TDNPGL.Native.Extensions/native_dll_to_res.py
@@ -1,10 +1,22 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 from shutil import copyfile
+import os
+
 def cp(frm,to):
 	try:
 		copyfile(frm,to)
 	except:
 		print(frm+" not exists!")
+def mkdir(direc):
+  if not os.path.isdir(direc):
+    os.mkdir(direc)
+
+os.system("python3 ../TDNPGL.Native/build.py")
+
+mkdir("x64")
+mkdir("x86")
+mkdir("arm")
+mkdir("arm64")
 
 cp("../TDNPGL.Native/build/Win/Release/tdnpgl.dll","x64/tdnpgl.dll")
 cp("../TDNPGL.Native/build/Win/Release/tdnpgl.dll","x86/tdnpgl.dll")
