4b294517c228550a9de9635e1b8d4323b0803d00
zatrit <52380960+zatrit@users.noreply.github.com>
2021-02-03 11:44:26 +0300

    New icon

--- src/Tools/TDNPGL.Cli/make_dpkg.py
+++ src/Tools/TDNPGL.Cli/make_dpkg.py
@@ -19,28 +19,19 @@ mkdir("./bin")
 mkdir("./bin/tdnpgl-deb")
 mkdir("./bin/tdnpgl-deb/usr")
 mkdir("./bin/tdnpgl-deb/usr/bin")
-mkdir("./bin/tdnpgl-deb/usr/lib")
-mkdir("./bin/tdnpgl-deb/usr/lib/tdnpgl")
+mkdir("./bin/tdnpgl-deb/usr/share")
+mkdir("./bin/tdnpgl-deb/usr/share/tdnpgl")
 mkdir("./bin/tdnpgl-deb/DEBIAN")
-if not os.path.exists("bin/tdnpgl-deb/usr/lib/tdnpgl"):
-	os.mkdir("./bin")
-    os.mkdir("./bin/tdnpgl-deb")
-    os.mkdir("./bin/tdnpgl-deb/usr")
-    os.mkdir("./bin/tdnpgl-deb/usr/bin")
-    os.mkdir("./bin/tdnpgl-deb/usr/lib")
-    os.mkdir("./bin/tdnpgl-deb/usr/lib/tdnpgl")
-    os.mkdir("./bin/tdnpgl-deb/DEBIAN")
->>>>>>> Updated CLI, Added more tests, updated debugging in core:src/Tools/TDNPGL.Cli/create_dpkg.py
 
 is_win=sys.platform=="win32"
 if(is_win):
     cpcmd="copy"
     release_path=".\\bin\\Release\\netcoreapp3.1\\*"
-    out_path="bin\\tdnpgl-deb\\usr\\lib\\tdnpgl\\"
+    out_path="bin\\tdnpgl-deb\\usr\\share\\tdnpgl\\"
 else:
     cpcmd="cp"
     release_path="./bin/Release/netcoreapp3.1/*"
-    out_path="./bin/tdnpgl-deb/usr/lib/tdnpgl/"
+    out_path="./bin/tdnpgl-deb/usr/share/tdnpgl/"
 
 os.system(cpcmd+" "+release_path+" "+out_path+" -r -f")
 
@@ -49,8 +40,8 @@ f.write("Package: tdnpgl\nVersion: "+ver[0]+"\nArchitecture: all\nEssential: no\
 f.close()
 
 f2 = open("./bin/tdnpgl-deb/usr/bin/tdnpgl","w")
-f2.write("#!/bin/bash\ndotnet /usr/lib/tdnpgl/tdnpgl.dll $*")
+f2.write("#!/bin/bash\ndotnet /usr/share/tdnpgl/tdnpgl.dll $*")
 f2.close()
 if not is_win:
-    os.system("chmod +x ./bin/tdnpgl-deb/usr/bin/tdnpgl")
+    os.system("chmod 755 ./bin/tdnpgl-deb/")
     os.system("dpkg -b ./bin/tdnpgl-deb/ ./bin/tdnpgl-any-cpu-1.5.4.4.deb")
