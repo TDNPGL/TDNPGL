f418bda7a60564d7d6ae09403841a2435d41b640
zatrit <zatrit@gmail.com>
2021-02-20 16:16:24 +0300

    Removed scripts signatures

---
+++ src/TDNPGL.Core/Graphics/Renderers/BaseGameRenderer.cs
@@ -0,0 +1,44 @@
+﻿using SkiaSharp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using TDNPGL.Core.Math;
+
+namespace TDNPGL.Core.Graphics.Renderers
+{
+    public sealed partial class BaseGameRenderer : IGameRenderer
+    {
+        public BaseGameRenderer(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+
+            renderer = new BaseLevelRenderer();
+        }
+
+        public double RenderWidth => width;
+        private double width;
+
+        public double RenderHeight => height;
+        private double height;
+
+        public double PixelSize => ScreenCalculations.OptimalPixelSize(width,height);
+
+        public ILevelRenderer LevelRenderer=>renderer;
+        private ILevelRenderer renderer;
+
+        public SKBitmap CurrentBitmap=>sKBitmap;
+        private SKBitmap sKBitmap;
+
+        public void Dispose()
+        {
+            sKBitmap.Dispose();
+        }
+
+        public void RenderBitmap(SKBitmap bitmap)
+        {
+            sKBitmap = bitmap;
+        }
+    }
+}
---
+++ src/Views/TDNPGL.Views.GLFW3/GLFWRenderWindow.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Reflection;
+using glfw3;
+using SkiaSharp;
+using TDNPGL.Core;
+using TDNPGL.Core.Gameplay.Assets;
+using TDNPGL.Core.Gameplay.Interfaces;
+using TDNPGL.Core.Graphics.Renderers;
+using TDNPGL.Core.Sound;
+
+namespace TDNPGL.Views.GLFW3
+{
+    public class GLFWRenderWindow : IGameRenderer, IGameInitializer, ISoundProvider
+    {
+        public GLFWRenderWindow()
+        {
+            if (!(Glfw.Init()==1))
+                Environment.Exit(-1);
+            var mon = Glfw.GetPrimaryMonitor();
+            wwindow = Glfw.CreateWindow(200,100,"Test",mon,new GLFWwindow(200,100,"Test"));
+            Glfw.MakeContextCurrent(wwindow);
+            wwindow.Show();
+        }
+
+        private GLFWwindow wwindow;
+        public Game game { get; set; }
+
+        public double RenderWidth => wwindow.GetSize().Item1;
+
+        public double RenderHeight => wwindow.GetSize().Item2;
+
+        public double PixelSize => throw new NotImplementedException();
+
+        public ILevelRenderer LevelRenderer => throw new NotImplementedException();
+
+        public Game CreateGame(Assembly assembly, string GameName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Game CreateGame<EntryType>(string GameName) where EntryType : EntryPoint
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PlaySound(SoundAsset asset, bool sync)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RenderBitmap(SKBitmap bitmap)
+        {
+            VkResult res=Glfw.CreateWindowSurface(wwindow.__Instance, IntPtr.Zero, IntPtr.Zero, 0);
+            
+        }
+    }
+}
